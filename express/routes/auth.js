fbd3b85a882ac30656218dc9abca49b97c82dc44
sumithkumar123 <147124944+sumithkumar123@users.noreply.github.com>
2025-04-01 21:32:22 +0530

    Delete express directory

--- express/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-// const { registerUser, loginUser } = require('../controllers/authController');
-const { registerUser, loginUser } = require('../controllers/authControllers');
-// Register user
-router.post('/register', registerUser);
-// Login user
-router.post('/login', loginUser);
-module.exports = router;
